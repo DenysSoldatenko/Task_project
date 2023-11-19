4a7e93a22f4b398ade2f8f578a12579edefade03
DenysSoldatenko <soldatenko911@gmail.com>
2023-11-19 08:00:00 +0300

    Modified dto

--- src/main/java/com/example/taskmanagerproject/dtos/TaskDto.java
+++ src/main/java/com/example/taskmanagerproject/dtos/TaskDto.java
@@ -2,11 +2,35 @@ package com.example.taskmanagerproject.dtos;
 
 import com.example.taskmanagerproject.entities.TaskStatus;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 
 /**
  * Represents a task DTO (Data Transfer Object) in the project.
  */
-public record TaskDto(Long id, String title, String description,
-                      TaskStatus taskStatus, LocalDateTime expirationDateTime) {
-}
+public record TaskDto(
+  Long id,
+
+  @NotNull(message = "Title cannot be null!")
+  @NotBlank(message = "Title cannot be blank!")
+  @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters long!")
+  String title,
+
+  @NotNull(message = "Description cannot be null!")
+  @NotBlank(message = "Description cannot be blank!")
+  @Size(min = 2, max = 255, message = "Description must be between 2 and 255 characters long!")
+  String description,
+
+  @NotNull(message = "Task status cannot be null!")
+  TaskStatus taskStatus,
+
+  @NotNull(message = "Expiration date and time cannot be null!")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  LocalDateTime expirationDateTime
+) {}
--- src/main/java/com/example/taskmanagerproject/dtos/UserDto.java
+++ src/main/java/com/example/taskmanagerproject/dtos/UserDto.java
@@ -1,11 +1,38 @@
 package com.example.taskmanagerproject.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.ToString;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 /**
  * Represents a user DTO (Data Transfer Object) in the project.
  */
-public record UserDto(Long id, String fullName, String username,
-                      @ToString.Include(name = "password") String password,
-                      @ToString.Include(name = "confirmPassword") String confirmPassword) {
-}
+public record UserDto(
+  Long id,
+
+  @NotNull(message = "Full name cannot be null!")
+  @NotBlank(message = "Full name cannot be blank!")
+  @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters long!")
+  String fullName,
+
+  @NotNull(message = "Username cannot be null!")
+  @NotBlank(message = "Username cannot be blank!")
+  @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters long!")
+  String username,
+
+  @NotNull(message = "Password cannot be null!")
+  @NotBlank(message = "Password cannot be blank!")
+  @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters long!")
+  @ToString.Include(name = "password")
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  String password,
+
+  @NotNull(message = "Confirm password cannot be null!")
+  @NotBlank(message = "Confirm password cannot be blank!")
+  @Size(min = 6, max = 20, message = "Confirm password must be between 6 and 20 characters long!")
+  @ToString.Include(name = "confirmPassword")
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  String confirmPassword
+) {}
