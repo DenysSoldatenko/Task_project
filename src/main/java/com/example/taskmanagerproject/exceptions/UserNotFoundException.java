389087d88076634209f92a83dc2ed08ea64665ea
DenysSoldatenko <soldatenko911@gmail.com>
2023-11-26 18:00:00 +0300

    Implemented user service

---
+++ src/main/java/com/example/taskmanagerproject/exceptions/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagerproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception class representing a not found error for users in the project.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/example/taskmanagerproject/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/taskmanagerproject/services/impl/UserServiceImpl.java
@@ -1,38 +1,73 @@
 package com.example.taskmanagerproject.services.impl;
 
+import com.example.taskmanagerproject.entities.Role;
 import com.example.taskmanagerproject.entities.User;
+import com.example.taskmanagerproject.exceptions.TaskNotFoundException;
+import com.example.taskmanagerproject.exceptions.UserNotFoundException;
+import com.example.taskmanagerproject.repositories.UserRepository;
 import com.example.taskmanagerproject.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
   @Override
+  @Transactional(readOnly = true)
   public User getUserById(Long userId) {
-    return null;
+    return userRepository.getUserById(userId)
+      .orElseThrow(() -> new UserNotFoundException("User not found."));
   }
 
   @Override
+  @Transactional(readOnly = true)
   public User getUserByUsername(String username) {
-    return null;
+    return userRepository.getUserByUsername(username)
+      .orElseThrow(() -> new UserNotFoundException("User not found."));
   }
 
   @Override
+  @Transactional
   public User updateUser(User user) {
-    return null;
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.updateUser(user);
+    return user;
   }
 
   @Override
+  @Transactional
   public User createUser(User user) {
-    return null;
+    if (userRepository.getUserByUsername(user.getUsername()).isPresent()) {
+      throw new IllegalStateException("User already exists.");
+    }
+    if (!user.getPassword().equals(user.getConfirmPassword())) {
+      throw new IllegalStateException("Password and password confirmation do not match.");
+    }
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.createUser(user);
+    Set<Role> roles = Set.of(Role.ROLE_USER);
+    userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
+    user.setUserRoles(roles);
+    return user;
   }
 
   @Override
+  @Transactional(readOnly = true)
   public boolean isUserTaskOwner(Long userId, Long taskId) {
-    return false;
+    return userRepository.isTaskOwner(userId, taskId);
   }
 
   @Override
+  @Transactional
   public void deleteUserById(Long userId) {
-
+    userRepository.deleteUserById(userId);
   }
 }
