b1acbae7b545084b8ce2f46ca98cb71c5b6f0cf4
DenysSoldatenko <soldatenko911@gmail.com>
2023-12-10 10:30:00 +0300

    Added tests to authentication service

--- src/test/java/com/example/taskmanagerproject/TaskManagerProjectApplicationTests.java
+++ src/test/java/com/example/taskmanagerproject/TaskManagerProjectApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.taskmanagerproject;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.taskmanagerproject.services.impl.AuthenticationServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskManagerProjectApplicationTests {
 
+	@Autowired
+	private AuthenticationServiceImpl authenticationService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(authenticationService);
 	}
-
 }
